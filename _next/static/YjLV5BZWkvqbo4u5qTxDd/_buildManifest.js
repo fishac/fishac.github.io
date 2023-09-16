5bf34d4269b2bc6af81ead9501574a2001b7421d
Alex Fish <acfishx@gmail.com>
2023-09-16 00:00:35 -0500

    Updates

---
+++ _next/static/YjLV5BZWkvqbo4u5qTxDd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/c16184b3-9c3d7cadd305e474.js","static/chunks/884-6ad3d30df607d315.js","static/chunks/pages/index-98ac45edbdd66d38.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
